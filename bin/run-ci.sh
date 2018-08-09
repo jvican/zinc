d3b34bc0945442a15b75725fe34971e31b6e4909
jvican <jorgevc@fastmail.es>
2018-08-09 11:09:17 +0200

    Add CI to scalacenter/zinc
    We add a CI to test our changes in our infrastructure before merging
    upstream in sbt/zinc.

--- bin/run-ci.sh
+++ bin/run-ci.sh
@@ -6,9 +6,8 @@ PROJECT_ROOT="zincRoot"
 sbt -Dfile.encoding=UTF-8 \
   -J-XX:ReservedCodeCacheSize=512M \
   -J-Xms1024M -J-Xmx4096M -J-server \
-  "$PROJECT_ROOT/mimaReportBinaryIssues" \
-  scalafmtCheck \
-  whitesourceCheckPolicies \
+  scalafmt::test \
+  test:scalafmt::test \
   "$PROJECT_ROOT/test:compile" \
   crossTestBridges \
   "$PROJECT_ROOT/test" \
