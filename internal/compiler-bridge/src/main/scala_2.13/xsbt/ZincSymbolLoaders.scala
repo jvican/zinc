d4354be32ad76502759eb29c1030b019de5ab1af
Jorge Vicente Cantero <jorgevc@fastmail.es>
2020-06-30 10:54:22 +0200

    Work around Scala 2.13.3 symbol loader breakage

--- internal/compiler-bridge/src/main/scala_2.13/xsbt/ZincSymbolLoaders.scala
+++ internal/compiler-bridge/src/main/scala_2.13/xsbt/ZincSymbolLoaders.scala
@@ -10,23 +10,34 @@ trait ZincSymbolLoaders extends GlobalSymbolLoaders with ZincPickleCompletion {
   import scala.tools.nsc.util.ClassRepresentation
   val invalidatedClassFilePaths: mutable.HashSet[String] = new mutable.HashSet[String]()
 
+  private var nameCharBuffer0 = new Array[Char](256)
   override def initializeFromClassPath(owner: Symbol, classRep: ClassRepresentation): Unit = {
+    def nameOf(classRep: ClassRepresentation): TermName = {
+      while (true) {
+        val len = classRep.nameChars(nameCharBuffer0)
+        if (len == -1) nameCharBuffer0 = new Array[Char](nameCharBuffer0.length * 2)
+        else return newTermName(nameCharBuffer0, 0, len)
+      }
+      throw new IllegalStateException()
+    }
+
+    val classRepName = nameOf(classRep)
     ((classRep.binary, classRep.source): @unchecked) match {
       case (Some(bin), Some(src))
-          if platform.needCompile(bin, src) && !binaryOnly(owner, classRep.name) =>
+          if platform.needCompile(bin, src) && !binaryOnly(owner, classRepName) =>
         if (settings.verbose) inform("[symloader] picked up newer source file for " + src.path)
-        enterToplevelsFromSource(owner, classRep.name, src)
+        enterToplevelsFromSource(owner, classRepName, src)
       case (None, Some(src)) =>
         if (settings.verbose) inform("[symloader] no class, picked up source file for " + src.path)
-        enterToplevelsFromSource(owner, classRep.name, src)
+        enterToplevelsFromSource(owner, classRepName, src)
       case (Some(bin), _) =>
         val classFile: File = bin.file
         if (classFile != null && invalidatedClassFilePaths.contains(classFile.getCanonicalPath)) {
           () // An invalidated class file should not be loaded
         } else if (bin.path.startsWith("_BPICKLE_")) {
-          enterClassAndModule(owner, classRep.name, new ZincPickleLoader(bin, _, _))
+          enterClassAndModule(owner, classRepName, new ZincPickleLoader(bin, _, _))
         } else {
-          enterClassAndModule(owner, classRep.name, new ClassfileLoader(bin, _, _))
+          enterClassAndModule(owner, classRepName, new ClassfileLoader(bin, _, _))
         }
     }
   }
